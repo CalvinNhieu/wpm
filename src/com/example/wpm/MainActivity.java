e8adfa234a4c8c2fbe63b309e7527aa772e020e8
Calvinhieu <calvin.nhieu@gmail.com>
2014-01-11 15:33:36 -0500

    Built main activity skeleton + test screen activity.

--- src/com/example/wpm/MainActivity.java
+++ src/com/example/wpm/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.wpm;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,12 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    // to change to test screen
+    public void startTest(View view) {
+        Intent toTestScreen = new Intent(this, TestScreen.class);
+        startActivity(toTestScreen);
     }
     
 }
