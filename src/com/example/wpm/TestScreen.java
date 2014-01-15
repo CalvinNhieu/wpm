6b426f03f63ee8a019012f87b169d29aa9708135
Calvinhieu <calvin.nhieu@gmail.com>
2014-01-14 22:52:39 -0500

    First attempt (failed) at logging test results in app data

--- src/com/example/wpm/TestScreen.java
+++ src/com/example/wpm/TestScreen.java
@@ -1,5 +1,8 @@
 package com.example.wpm;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -10,6 +13,7 @@ import org.jsoup.nodes.Document;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -32,10 +36,12 @@ public class TestScreen extends Activity {
 	AlertDialog alert1; // notification window
 	
 	int correct; // counts # of correct words typed
-	int wrong; // counts # of wrong words typed
+	int mistakes; // counts # of wrong words typed
 	String passage; // contains article scraped from web
+	String logFileName; // name of log file
 	long startTime; // time test is started
 	long timeElapsed; // time test took to complete
+	double t; // double type of timeElapsed
 	double wpm; // user's wpm result
 	boolean finished; // whether or not test is complete
 	
@@ -93,8 +99,9 @@ public class TestScreen extends Activity {
 		tester = new Scanner(passageDisplay.getText().toString());
 		tester.useDelimiter(" ");
 		passageEntries = new ArrayList <String>();
+		logFileName = "log.txt";
 		correct = 0;
-		wrong = 0;
+		mistakes = 0;
 		startTime = 0;
 		timeElapsed = 0;
 		finished = false;
@@ -119,7 +126,7 @@ public class TestScreen extends Activity {
 				  if (s.length() < 1) {
 					  return;
 				  }
-				  if(startTime==0 && correct == 0 && wrong == 0) {
+				  if(startTime==0 && correct == 0 && mistakes == 0) {
 					  startTime = System.currentTimeMillis();
 				  }
 				  
@@ -134,7 +141,7 @@ public class TestScreen extends Activity {
 						  correct++;
 					  }
 					  else {
-						  wrong++;
+						  mistakes++;
 					  }
 					  inputField.setText("");
 				  }
@@ -159,14 +166,26 @@ public class TestScreen extends Activity {
 	public void endTest (long time, int numOfWords) {
 		finished = true;
 		if (time < 600000) {
-			double t = (double) time;
+			t= (double) time;
 			t /= 60000;
 			wpm = (double) (t*numOfWords);
 		
 			builder1 = new AlertDialog.Builder(this);
-			builder1.setMessage("Your WPM is: " + wpm);
+			builder1.setMessage("Results: " + wpm + " wrds/min");
 			builder1.setCancelable(true);
 			builder1.setPositiveButton("Send to Log",
+					new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.cancel();
+					try {
+						sendToLog(wpm,t*60,correct,mistakes);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					toMain();
+				}
+			});
+			builder1.setNegativeButton("Trash",
 					new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int id) {
 					dialog.cancel();
@@ -189,6 +208,27 @@ public class TestScreen extends Activity {
 		alert1 = builder1.create();
         alert1.show();
 	}
+	
+	// OK SO THIS DOES NOT WORK . I don't know what to do to log this business lol
+	public void sendToLog(double wpm, double timeInSeconds, int totalWords, int mistakes) throws IOException {
+		// write the file if it doesn't already exist
+		//if (!fileExistance(logFileName)) { I DONT KNOW IF WE NEED THIS LOL
+			String logInfo = wpm + "|" + timeInSeconds + "|" + totalWords + "|" + mistakes + "/";
+			FileOutputStream writer = openFileOutput (logFileName, Context.MODE_PRIVATE);
+			writer.write(logInfo.getBytes());
+			writer.close();
+			
+			FileInputStream reader = openFileInput(logFileName);
+			System.out.println(reader.read());
+		//}
+	}
+	
+	public boolean fileExistance(String fname){
+	    File file = getBaseContext().getFileStreamPath(fname);
+	    return file.exists();
+	}
+	
+	
 	
 	// return to main menu method
 	public void toMain () {
